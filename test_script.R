5e1a888800bcc279d2ba26039d9cb1241b3d6145
aast242 <85575864+aast242@users.noreply.github.com>
2023-11-02 10:13:36 -0700

    added code

--- test_script.R
+++ test_script.R
@@ -4,3 +4,31 @@
 
 # Loading packages --------------------------------------------------------
 library(tidyverse) # for all the tidythings
+
+
+#Categorical
+
+n_nests <- 30
+rand_na = n_nests * 0.1
+
+color_vals = c("nocolor", "green")
+
+d1 <- data.frame(nest = 1:n_nests,
+                 treatment = c(rep("water", n_nests),
+                               rep("permethrin", n_nests)))
+
+d1$parasites[d1$treatment == "water"] <- rpois(n_nests, 20)
+d1$parasites[d1$treatment == "permethrin"] <- rpois(n_nests, 3)
+d1
+# write.csv(d1, "parasite_data.csv")
+
+
+#Continuous
+n_nests <- 30
+d2 <- data.frame(nest = 1:n_nests,
+                 cotton = runif(n_nests, min = 0, max = 8),
+                 error = rnorm(n_nests, mean = 0, sd = 2))
+
+d2 <- mutate(d2, parasites = 20 -(cotton * 2.1) + error)
+
+plot(parasites ~ cotton, d2)
